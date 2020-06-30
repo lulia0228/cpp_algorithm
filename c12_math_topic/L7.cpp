55bfe86be286c00d4b4ec38d10e250940a472ba7
lulia <liheng_lulia@163.com>
2020-06-30 14:31:25 +0800

    change math

--- c12_math_topic/L7.cpp
+++ c12_math_topic/L7.cpp
@@ -7,24 +7,24 @@
 #include <cmath>
 using namespace std ;
 // 32位 [-2147483648 , 2147483647]
-class Solution{
+class Solution {
 public:
-    // leetcode 7 反转整数
-    int reverseInt( int num ) {
-        long long int result = num % 10; //必须要用long long int
-
-        for (; num /= 10;) {   //循环中执行condition中num值顺便被改变了
-            result = result * 10 + num % 10;
+    int reverse(int x) {
+        long long res = 0;
+        while(x){
+            res = res*10 + x%10;
+            x /= 10;
         }
-        return (result < INT32_MIN || result > INT32_MAX)? 0:result;
+        if(res < INT32_MIN || res > INT32_MAX)
+            return 0;
+        return res;
     }
-
 
 };
 
 
  int main(){
-    int re = Solution().reverseInt(-534236469);
+    int re = Solution().reverse(-534236469);
     cout << re << endl  ;
     return 0;
 
