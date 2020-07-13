964d7e2c168c8c328bc533ca6d7289ad6d118441
lulia <liheng_lulia@163.com>
2020-07-13 15:20:01 +0800

    add toposort

--- c14_graph_toposort/L207-m.cpp
+++ c14_graph_toposort/L207-m.cpp
@@ -6,6 +6,12 @@
 #include <vector>
 using  namespace std;
 
+//本题可约化为： 课程安排图是否是 有向无环图(DAG)。即课程间规定了前置条件，但不能构成任何环路，否则课程前置条件将不成立。
+//思路：通过拓扑排序判断此课程安排图是否是 有向无环图(DAG) 。
+//拓扑排序原理： 对 DAG 的顶点进行排序，使得对每一条有向边(u,v)，均有u（在排序记录中）比v先出现。
+//亦可理解为对某点v而言，只有当v的所有源点均出现了，v才能出现。
+
+
 //方法1 DFS 对每个节点做DFS判断是否有环
 class Solution {
 public:
@@ -13,27 +19,27 @@ public:
         vector<int> flag(numCourses, 0);
         vector<vector<int>> adjacency(numCourses, vector<int>());
         for(auto vec: prerequisites)
-            //adjacency[vec[1]].push_back(vec[0]);
-            adjacency[vec[0]].push_back(vec[1]); //使用DFS方法，两种存放都可以
+            adjacency[vec[1]].push_back(vec[0]); //这里注意是有向图
         for(int i=0; i<numCourses; ++i)
-            if (!dfs(i, flag, adjacency))
+            if (dfs(i, flag, adjacency))
                 return false;
         return true;
     }
 
+    //返回true代表有向图 有环，false代表无环
     bool dfs(int index, vector<int>& flag, vector<vector<int>>& adjacency){
-        if(flag[index] == -1) return true;
-        if(flag[index] == 1) return false;
+        if(flag[index] == -1) return false; //提前剪枝，说明以当前index为起始点的环检测前面已经做过了，而且是没有环的
+        if(flag[index] == 1) return true;
         flag[index] = 1;
-        for(auto j:adjacency[index]) //注意adjacency[index]里面存放的是index节点的所有邻接节点的索引，直接使用即可
-            if (!dfs(j, flag, adjacency))
-                return false;
-        flag[index] = -1;
-        return true;
+        for(auto j:adjacency[index]) //adjacency[index]里面存放的是学完index课程后可以学的其他课程
+            if (dfs(j, flag, adjacency))
+                return true;
+        flag[index] = -1; //说明index起始检测没环,这句话必不可少：可以用案例3 [[0,1],[0,2],[1,2]] 理解
+        return false;
     }
 };
 
-//方法2 同样是判断图中有无环存在  但是使用的是   拓扑排序+宽搜
+//方法2 同样是判断有向图中有无环存在  但是使用的是bfs实现拓扑排序
 #include <queue>
 class Solution1 {
 public:
@@ -53,12 +59,12 @@ public:
             q1.pop();
             numCourses -= 1;
             for(auto cur:adjacency[pre]){
-                indegrees[cur] --;
+                indegrees[cur]--;
                 if(indegrees[cur] == 0)
                     q1.push(cur);
             }
         }
-        return numCourses==0;
+        return numCourses == 0;
     }
 };
 
--- c14_graph_toposort/L210-m.cpp
+++ c14_graph_toposort/L210-m.cpp
@@ -2,6 +2,8 @@
 // Created by LiHeng on 2020/7/12.
 //
 
+//DFS实现拓扑排序
+
 #include <iostream>
 #include <vector>
 using  namespace std;
@@ -12,24 +14,58 @@ public:
         vector<int> flag(numCourses, 0);
         vector<vector<int>> adjacency(numCourses, vector<int>());
         for(auto vec: prerequisites)
-            //adjacency[vec[1]].push_back(vec[0]);
             adjacency[vec[1]].push_back(vec[0]);
         for(int i=0; i<numCourses; ++i)
-            if (!dfs(i, flag, adjacency))
+            if (dfs(i, flag, adjacency))
                 return vector<int>();
         reverse(res.begin(), res.end()); //为什么要反序，思考下
         return res;
     }
 
+    //返回true代表有向图有环，false代表无环
     bool dfs(int index, vector<int>& flag, vector<vector<int>>& adjacency){
-        if(flag[index] == -1) return true;
-        if(flag[index] == 1) return false;
+        if(flag[index] == -1) return false;
+        if(flag[index] == 1) return true;
         flag[index] = 1;
-        for(auto j:adjacency[index]) //注意adjacency[index]里面存放的是index节点的所有邻接节点
-            if (!dfs(j, flag, adjacency))
-                return false;
+        for(auto j:adjacency[index]) //adjacency[index]里面存放的是学完index课程后可以学的其他课程
+            if (dfs(j, flag, adjacency))
+                return true;
         flag[index] = -1;
         res.push_back(index);
-        return true;
+        return false;
+    }
+};
+
+//可以用案例 3 [[0,1],[0,2],[1,2]] 理解过程
+
+
+//BFS实现的拓扑排序
+#include <queue>
+class Solution1 {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> res;
+        vector<int> indegrees(numCourses, 0); //入度记录
+        vector<vector<int>> adjacency(numCourses, vector<int>());
+        for(auto vec: prerequisites){
+            indegrees[vec[0]]++;
+            adjacency[vec[1]].push_back(vec[0]);
+        }
+        queue<int> q1;
+        for(int i=0; i<numCourses; i++)
+            if(indegrees[i] == 0)
+                q1.push(i);
+        while(!q1.empty()){
+            int pre = q1.front();
+            q1.pop();
+            res.push_back(pre);
+            numCourses -= 1;
+            for(auto cur:adjacency[pre]){
+                indegrees[cur]--;
+                if(indegrees[cur] == 0)
+                    q1.push(cur);
+            }
+        }
+        return numCourses == 0?res:vector<int>();
     }
 };
