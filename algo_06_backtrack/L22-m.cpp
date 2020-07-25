8933fc32375443e2b793768621e085aac6d37460
trail 1 <877598323@qq.com>
2020-07-25 16:56:34 +0800

    xifen tree

--- algo_06_backtrack/L22-m.cpp
+++ algo_06_backtrack/L22-m.cpp
@@ -159,3 +159,82 @@ public:
 };
 
 //以上5种做法还是DP最快
+
+
+//1.1 递归：DFS
+class Solution6 {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        string ss;
+        _generate(0, 0, n, ss);
+        return res;
+    }
+    //lt rt 代表已经使用的左右括号个数
+    void _generate(int lt, int rt, int n, string s){
+        if(lt==n && rt==n){
+            res.push_back(s);
+            return;
+        }
+        if(lt < n) //已经使用的左括号个数小于n
+            _generate(lt+1, rt, n, s+"(");
+        if(lt > rt) //左括号个数大于有括号才可以添加右括号
+            _generate(lt, rt+1, n, s+")");
+    }
+};
+
+//1.2 递归：DFS
+class Solution7 {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        if (n == 0)
+            return res;
+        dfs("", n, n, res);
+        return res;
+    }
+    //和1.1反着来，left right表示剩下的可供使用的括号数
+    void dfs(string cur, int left, int right, vector<string>& res){
+        if(left == 0 && right== 0){
+            res.push_back(cur);
+            return;
+        }
+        if(left > right) //剪枝
+            return;
+        if(left > 0)
+            dfs(cur+'(', left-1, right, res);
+        if(right > 0)
+            dfs(cur+')', left, right-1, res);
+    }
+};
+
+//2 BFS 设计队列完成（速度会慢下来）
+#include <queue>
+struct Node {
+    string s;
+    int left;
+    int right;
+    Node(string s, int l, int r) : s(s), left(l), right(r) {}
+};
+
+class Solution8{
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        if (n == 0)
+            return res;
+        queue<Node> q1;
+        q1.push(Node("", n, n));
+        while(!q1.empty()){
+            Node cur_node = q1.front();
+            q1.pop();
+            if(cur_node.left == 0 && cur_node.right == 0)
+                res.push_back(cur_node.s);
+            if(cur_node.left > 0)
+                q1.push(Node(cur_node.s+'(', cur_node.left-1, cur_node.right));
+            if(cur_node.right > 0 && cur_node.left < cur_node.right)
+                q1.push(Node(cur_node.s+')', cur_node.left, cur_node.right-1));
+        }
+        return res;
+    }
+};
--- algo_07_divide_and_conquer/L241-m.cpp
+++ algo_07_divide_and_conquer/L241-m.cpp
@@ -1,4 +1,45 @@
 //
 // Created by LiHeng on 2020/7/25.
 //
-
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> res;
+        for(int i = 0; i < input.size(); ++i){
+            char c = input[i];
+            if(c == '+'|| c == '-' || c == '*'){//递归划分
+                auto res1 = diffWaysToCompute(input.substr(0, i));
+                auto res2 = diffWaysToCompute(input.substr(i+1));
+                for(auto r1 : res1){//计算结果
+                    for(auto r2 : res2){
+                        // if(c == '+'){
+                        //     res.push_back(r1 + r2);
+                        // } else if(c == '-'){
+                        //     res.push_back(r1 - r2);
+                        // } else if(c == '*'){
+                        //     res.push_back(r1 * r2);
+                        // }
+                        switch (c) {
+                            case '+':
+                                res.push_back(r1 + r2);
+                                break;
+                            case '-':
+                                res.push_back(r1 - r2);
+                                break;
+                            case '*':
+                                res.push_back(r1 * r2);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        if(res.empty()){//纯数字，input中没符号
+            res.push_back(stoi(input));
+        }
+        return res;
+    }
+};
