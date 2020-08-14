a30735e180787bfb932effbca6f69132d30fb191
lulia <liheng_lulia@163.com>
2020-08-14 12:51:38 +0800

    add python 328

---
+++ python-algorithm/ds_4_linkedlist/linkedlist_2_inplace_operation/L328-m.py
@@ -0,0 +1,27 @@
+#--coding:utf-8--
+'''
+@Time   : 2020/8/14
+@Author : Heng Li
+@Email  : liheng_lulia@163.com
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        flag = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = flag
+        return head
