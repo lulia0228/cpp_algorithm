be8743d4320238ac40e5d5d40ec020a163d80886
trail 1 <877598323@qq.com>
2020-09-19 20:54:03 +0800

    add 904 python

---
+++ python-algorithm/algo_02_sliding_window/L904-m.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/19 20:38
+# @Author  : Heng Li
+# @File    : L904-m.py
+# @Software: PyCharm
+
+# 1 超时 收缩窗口的条件太费时间
+class Solution:
+    def totalFruit(self, tree: List[int]) -> int:
+        lt, rt = 0, 0
+        res = 1
+        while rt < len(tree):
+            # 开始收缩窗口
+            while len(set(tree[lt:rt+1])) > 2:
+                lt += 1
+            res = max(res, rt-lt+1)
+            rt += 1
+        return res
+
+# 2  巧妙设计下,速度快多了
+class Solution:
+    def totalFruit(self, tree: List[int]) -> int:
+        lt, rt = 0, 0
+        res = 1
+        st = set()
+        while rt < len(tree):
+            st.add(tree[rt])
+            # 开始收缩窗口
+            if len(st) == 3: # 说明当前s[rt]一定是新增的种类的第一个元素
+                lt = rt-2;
+                while tree[lt] == tree[rt-1]:
+                    lt -= 1
+                st.remove(tree[lt]);
+                lt += 1
+            else:
+                res = max(res, rt-lt+1)
+            rt += 1
+        return res
