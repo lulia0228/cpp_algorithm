9e0b2c92e43dbc3e26fb089204c9a59469e8c3d8
trail 1 <877598323@qq.com>
2020-10-03 10:07:24 +0800

    add

---
+++ python-algorithm/ds_6_string/L151-m.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/3 10:01
+# @Author  : No Name
+
+# 注意和题目557 不一样
+# 题目要求不得占用额外空间
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = ""
+        i = 0
+        while i<len(s):
+            word = ""
+            while i < len(s) and s[i] != ' ':
+                word += s[i]
+                i += 1
+            if word != "":
+                if res == "":
+                    res = word
+                else:
+                    res = word + ' ' + res
+            else:
+                i += 1
+
+        return res
+
+# 占用额外空间
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_l = s.split(' ')
+        s_l = [item for item in s_l if item not in ['']]
+        return ' '.join(s_l[::-1])
