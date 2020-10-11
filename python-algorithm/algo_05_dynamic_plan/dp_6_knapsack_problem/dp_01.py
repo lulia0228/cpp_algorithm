b9cd5f32e1b2794821ab360436ad25e9e9dbb870
liheng <877598323@qq.com>
2020-10-11 12:51:12 +0800

    change o1

--- python-algorithm/algo_05_dynamic_plan/dp_6_knapsack_problem/dp_01.py
+++ python-algorithm/algo_05_dynamic_plan/dp_6_knapsack_problem/dp_01.py
@@ -12,7 +12,7 @@ class Solution:
         n = len(w)
         # dp[i][j]表示用[0,...,i-1]物品填充容量为j的最大价值
         dp = [[0]*(C+1) for i in range(n+1)]
-        for i in range(1, n):
+        for i in range(1, n+1):
             for j in range(C+1):
                 # 装或者不装第i个
                 if j >= w[i]:
@@ -20,3 +20,14 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j]  # 不装第i个
         return dp[n][C]
+
+# 省空间的写法
+class Solution:
+    def knapsack(self, w , v , C ):
+        n = len(w)
+        dp = [0]*(C+1)
+        for i in range(n):
+            # 只有倒着设计才能保证每个物品只被使用1次
+            for j in range(C, w[i-1]-1, -1):
+                dp[j] = max(dp[j], v[i]+dp[j - w[i]])
+        return [C]
