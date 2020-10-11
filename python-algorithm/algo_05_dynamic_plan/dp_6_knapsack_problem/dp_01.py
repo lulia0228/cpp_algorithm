eb531fba754ae9f882054a702aa4f9a5d05c03a1
liheng <877598323@qq.com>
2020-10-11 10:47:00 +0800

    add python 01

--- python-algorithm/algo_05_dynamic_plan/dp_6_knapsack_problem/dp_01.py
+++ python-algorithm/algo_05_dynamic_plan/dp_6_knapsack_problem/dp_01.py
@@ -10,17 +10,13 @@
 class Solution:
     def knapsack(self, w , v , C ):
         n = len(w)
-        # dp[i][j]表示用[0,...,i]物品填充容量为j的最大价值
-        dp = [[-1]*(C+1) for i in range(n)]
-        for j in range(C+1):
-            dp[0][j] = (v[0] if j>w[0] else 0) # 即考虑第1个物品（索引是0）各个重量下的转态
-
+        # dp[i][j]表示用[0,...,i-1]物品填充容量为j的最大价值
+        dp = [[0]*(C+1) for i in range(n+1)]
         for i in range(1, n):
             for j in range(C+1):
                 # 装或者不装第i个
                 if j >= w[i]:
-                    dp[i][j] = max(dp[i-1][j], v[i]+dp[i-1][j - w[i]])
+                    dp[i][j] = max(dp[i-1][j], v[i-1]+dp[i-1][j - w[i-1]])
                 else:
                     dp[i][j] = dp[i-1][j]  # 不装第i个
-
-        return dp[n-1][C]
+        return dp[n][C]
