3807030f5f0ec5bd467e9f7577156dd8a4d5ce2f
lulia <liheng_lulia@163.com>
2020-10-15 17:59:42 +0800

    add tree

---
+++ python-algorithm/ds_5_tree/tree_1_4_path_recurrent/L337-m.py
@@ -0,0 +1,37 @@
+#--coding:utf-8--
+'''
+@Time   : 2020/10/15
+@Author : No Name
+'''
+
+class Solution:
+
+    dic = {} # 记忆化搜索
+
+    def rob(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        if root in self.dic:
+            return self.dic[root]
+        val1 = root.val
+        if root.left:
+            val1 += self.rob(root.left.left) + self.rob(root.left.right)
+        if root.right:
+            val1 += self.rob(root.right.left) + self.rob(root.right.right)
+        val2 = self.rob(root.left) + self.rob(root.right)
+        res = max(val1, val2)
+        self.dic[root] = res
+        return res
+
+
+# 动态规划  没咋看懂
+# ls表示偷左子树能带来的最大收益，ln表示不偷左子树能带来的最大收益，rs、rn同理
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def _rob(root):
+            if not root:
+                return 0, 0
+            ls, ln = _rob(root.left)
+            rs, rn = _rob(root.right)
+            return root.val + ln + rn, max(ls, ln) + max(rs, rn)
+        return max(_rob(root))
---
+++ python-algorithm/ds_5_tree/tree_1_4_path_recurrent/L437.py
@@ -0,0 +1,38 @@
+#--coding:utf-8--
+'''
+@Time   : 2020/10/15
+@Author : No Name
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 和判断子树572那道题的结构很类似
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        if root == None:
+            return 0
+        # 对于每个节点考虑三种情况：
+        # 1.路径包含这个节点
+        # 2.路径不包含这个节点，以当前节点的左子节点开头
+        # 3.路径不包含这个节点，以当前节点的右子节点开头
+        res = self.start_of_curnode(root, sum)
+        res += self.pathSum(root.left, sum)
+        res += self.pathSum(root.right, sum)
+        return res
+
+    # 返回以当前节点为根节点的子树的路径和我sum的个数
+    def start_of_curnode(self, root, sum):
+        if root == None:
+            return 0
+        res = 0
+        if root.val == sum:
+            res += 1
+        res += self.start_of_curnode(root.left, sum-root.val)
+        res += self.start_of_curnode(root.right, sum-root.val)
+        return res
