bbb1c098e2fc86d1ad4e1c957eb38f7ebeca9bd1
lulia <liheng_lulia@163.com>
2020-12-18 10:52:24 +0800

    update 59_1

--- python-algorithm/sword_point_offer/J59_1.py
+++ python-algorithm/sword_point_offer/J59_1.py
@@ -7,6 +7,8 @@
 # 1.窗口每向前滑动一次，如果当前队首元素等于num[i-1]将其删除
 # 2.保持队列不是单调递增；把队列中的所有小于新加的元素nums[j]的元素全部从队尾删除
 
+# 每次滑动一次相当于出队一个元素，入队一个元素
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         deque = collections.deque()
