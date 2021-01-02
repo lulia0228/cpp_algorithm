4724865e512272a44c246e90b0026497cb1b504c
liheng <877598323@qq.com>
2021-01-02 11:31:24 +0800

    update L8

--- python-algorithm/ds_6_string/L8-m.py
+++ python-algorithm/ds_6_string/L8-m.py
@@ -31,6 +31,47 @@ class Solution:
             idx += 1
         return res if sign==1 else -res
 
+# 有限状态机
+INT_MAX=2**31-1
+INT_MIN=-2**31
+
+class Automata:
+    def __init__(self):
+        self.state="start"
+        self.ans=0
+        self.sign=1
+        self.table={
+            "start":["start","sign","innum","end"],
+            "sign":["end","end","innum","end"],
+            "innum":["end","end","innum","end"],
+            "end":["end","end","end","end"],
+        }
+    def get_s(self,c):
+        if c.isspace():
+            return 0
+        if c=='+' or c=='-':
+            return 1
+        if c.isdigit():
+            return 2
+        return 3
+
+    def get(self,c):
+        self.state=self.table[self.state][self.get_s(c)]
+        if self.state=="innum":
+            self.ans=self.ans*10+int(c)
+            self.ans= min(self.ans,INT_MAX) if self.sign==1 else min(self.ans,-INT_MIN)
+        if self.state=="sign":
+            if c=='-':
+                self.sign=-1
+
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        Auto=Automata()
+        for c in str:
+            Auto.get(c)
+        return Auto.ans*Auto.sign
+
 p = Solution()
 s1 = "words and 987"
 s2 = " ++987"
