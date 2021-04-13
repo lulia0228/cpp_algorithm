a93078433927f2f37b48189105a20440001f4d0b
lulia <liheng_lulia@163.com>
2021-04-13 14:02:04 +0800

    update 206

--- python-algorithm/ds_4_linkedlist/linkedlist_2_inplace_operation/L206.py
+++ python-algorithm/ds_4_linkedlist/linkedlist_2_inplace_operation/L206.py
@@ -18,3 +18,14 @@ class Solution:
             back = head
             head = front
         return back
+
+
+# 递归法
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
