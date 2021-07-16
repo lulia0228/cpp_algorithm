5ea91a2dc5b4a219a7b64f2a1cdcb189b0f0cc81
lulia <liheng_lulia@163.com>
2021-07-16 15:32:36 +0800

    add new

---
+++ python-algorithm/NewCoder/NC132约瑟夫环.py
@@ -0,0 +1,26 @@
+#--coding:utf-8--
+# f(n, m)表示长度为n的序列，删除n-1次，最后留下元素的序号
+# f(n, m) = (f(n-1, m) + m) % n
+class Solution:
+    def ysf(self, n, m):
+        # write code here
+        res = []
+        for i in range(0, n):
+            res.append(i + 1)
+        i = 0
+        for j in range(0, n - 1):
+            i = (i + m - 1) % (n - j)
+            res.pop(i)
+        return res[0]
+    
+class Solution:
+    def ysf(self, n, m):
+        # write code here
+        if m < 1 or n < 1:
+            return -1
+        last = 0
+        # i代表有目前有个人
+        # 最后一轮剩下2个人，所以从2开始反推
+        for i in range(2, n+1):
+            last = (last + m) % i
+        return last+1
---
+++ python-algorithm/NewCoder/NC87丢棋子.py
@@ -0,0 +1,24 @@
+#--coding:utf-8--
+
+# dp[0][K] = 0, dp[N][1] = N, dp[N][K] = min(max(dp[i-1][K-1], dp[N-i][K])) + 1
+
+
+class Solution:
+    def solve(self , n , k ):
+        # write code here
+        if n<1 or k<1: return 0
+        if k==1: return n
+        dp = [[0]*(k+1) for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][1] = i
+        for i in range(1, n+1):
+            for j in range(2,k+1):
+                min_ = float("inf")
+                for c in range(1,i+1):
+                    min_ = min(min_, max(dp[c-1][j-1], dp[i-c][j]))
+                dp[i][j] = min_ + 1
+        return dp[n][k]
+
+n,k = 1000000,1000000
+res = Solution().solve(n, k)
+print(res) #
