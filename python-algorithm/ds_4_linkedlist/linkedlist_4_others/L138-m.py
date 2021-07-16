0b552c34be2cd03d5730397299fd86090f43fff3
lulia <liheng_lulia@163.com>
2021-07-16 14:40:34 +0800

    add new

--- python-algorithm/ds_4_linkedlist/linkedlist_4_others/L138-m.py
+++ python-algorithm/ds_4_linkedlist/linkedlist_4_others/L138-m.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 
 # 复制带随机指针的链表
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        Mydic = dict()
+        def recursion(node: 'Node') -> 'Node':
+            if node is None: return None
+            if node in Mydic: return Mydic.get(node)
+            root = Node(node.val)
+            Mydic[node] = root
+            root.next = recursion(node.next)
+            root.random = recursion(node.random)
+            return root
+        return recursion(head)
+
